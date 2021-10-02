f831ef890a9fea2887a3758999c1781f5415ac36
yousufzia <91566760+yousufzia@users.noreply.github.com>
2021-10-02 13:49:41 -0400

    Update DemoTeamOne.java

--- src/one/DemoTeamOne.java
+++ src/one/DemoTeamOne.java
@@ -2,7 +2,7 @@ package one;
 
 public class DemoTeamOne {
 
-	// comment from Git
+	// comment from Git from Wagma
 	public static void addNum(int a, int b, int c) {
 		System.out.println(a + b + c);
 	}
