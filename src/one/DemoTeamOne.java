473d42b80c15dcbdc639bbd6b2be67c2bdf4de62
jamilabensalem <jamilabensalem@localhost>
2021-10-02 13:54:11 -0400

    hello

--- src/one/DemoTeamOne.java
+++ src/one/DemoTeamOne.java
@@ -5,5 +5,7 @@ public class DemoTeamOne {
 	// comment from Git from Wagma
 	public static void addNum(int a, int b, int c) {
 		System.out.println(a + b + c);
+		
+		System.out.println("hello");
 	}
 }
